cd0d2a4f811553455e65fef18f3020a4632b176e
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2022-04-26 14:53:46 +0200

    added 'selected items array', clear all selected button + additional styling

--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -16,6 +16,7 @@ interface IProps {
 function SearchInput({ data, placeholder }: IProps) {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [filteredData, setFilteredData] = useState<IData[]>([]);
+  const [addedItems, setAddedItems] = useState<IData[]>([]);
 
   useEffect(() => {
     let newFilter = data.filter((value) => {
@@ -27,7 +28,7 @@ function SearchInput({ data, placeholder }: IProps) {
     } else {
       setFilteredData(newFilter);
     }
-  }, [searchTerm, data]);
+  }, [searchTerm, data, filteredData]);
 
   const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
@@ -35,6 +36,25 @@ function SearchInput({ data, placeholder }: IProps) {
 
   const clearInput = () => {
     setSearchTerm("");
+  };
+
+  const addItem = (value: IData) => {
+    setAddedItems([...addedItems, value]);
+    setSearchTerm("");
+  };
+
+  const removeItem = (value: IData) => {
+    let newArray: IData[] = addedItems;
+    for (let i = 0; i < addedItems.length; i++) {
+      if (value.id === newArray[i].id) {
+        newArray.splice(i, 1);
+      }
+    }
+    setAddedItems(newArray);
+  };
+
+  const clearAllItems = () => {
+    setAddedItems([]);
   };
 
   return (
@@ -59,11 +79,36 @@ function SearchInput({ data, placeholder }: IProps) {
         </div>
       </div>
 
+      {addedItems.length !== 0 && (
+        <div className="addedItemsContainer">
+          {addedItems.map((i) => {
+            return (
+              <div key={i.id} className="addedItem">
+                <p>{i.name}</p>
+                <MdOutlineClear
+                  className="removeItem"
+                  onClick={() => removeItem(i)}
+                />
+              </div>
+            );
+          })}
+          <div className="clearContainer">
+            <button className="clearAllItems" onClick={clearAllItems}>
+              Clear all
+            </button>
+          </div>
+        </div>
+      )}
+
       {filteredData.length !== 0 && (
         <div className="dataResult">
           {filteredData.slice(0, 10).map((value) => {
             return (
-              <p className="dataItem" key={value.id}>
+              <p
+                className="dataItem"
+                key={value.id}
+                onClick={() => addItem(value)}
+              >
                 {value.name}
               </p>
             );
